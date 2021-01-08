83f7029adaa0feada4ec9afb1450e85cfec52738
Nathan Schrader <nathanschrader@icloud.com>
2021-01-08 11:44:17 -0800

    add MachinesController namespace, class, Index handler

---
+++ DrSillystringz/Controllers/MachinesController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using DrSillystringz.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DrSillystringz.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly DrSillystringzContext _db;
+
+    public MachinesController(DrSillystringz db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+  }
+}
