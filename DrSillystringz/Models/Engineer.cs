f3168fe846e194ccc0f176c0c7dc69ecf06b479a
Nathan Schrader <nathanschrader@icloud.com>
2021-01-08 09:35:33 -0800

    add Engineer Model

---
+++ DrSillystringz/Models/Engineer.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace DrSillystringz.Models
+{
+  public class Engineer
+  }
+    public Engineer()
+    {
+      //this.Machines = new Hashset<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
+
+  }
+}
