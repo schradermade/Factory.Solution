25967dfabf39b21a7b03cdf6a406e08157f29df6
Nathan Schrader <nathanschrader@icloud.com>
2021-01-08 10:59:40 -0800

    add Details GET handler

--- DrSillystringz/Controllers/EngineersController.cs
+++ DrSillystringz/Controllers/EngineersController.cs
@@ -50,5 +50,14 @@ namespace DrSillystringz.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+          .Include(engineer => engineer.Machines)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return view(thisEngineer);
+    }
+
   }
 }
